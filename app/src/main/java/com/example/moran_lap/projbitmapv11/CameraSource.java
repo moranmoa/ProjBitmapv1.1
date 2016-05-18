0e4df56d677cb00a9c0d84e61132812cee044d52
gilibarak280 - Gili Barak <gilibarak280@gmail.com>
2016-05-18 20:23:50 +0300

    Gili - open camera code - to edit and add getImage();

--- app/src/main/java/com/example/moran_lap/projbitmapv11/CameraSource.java
+++ app/src/main/java/com/example/moran_lap/projbitmapv11/CameraSource.java
@@ -2,17 +2,26 @@ package com.example.moran_lap.projbitmapv11;
 
 import android.graphics.Bitmap;
 
+
 /**
  * Created by Moran-Lap on 23/04/2016.
  */
 public class CameraSource extends ImageSource {
 
-    public CameraSource(){
+    Camera2BasicFragment camera2BasicFragment;
+
+    public CameraSource() {
         sourceName = "Camera";
+        camera2BasicFragment = Camera2BasicFragment.newInstance();
+        ApplicationContext.getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.container, camera2BasicFragment)
+                .commit();
     }
+
 
     @Override
     public Bitmap getImage() {
+        //return Camera2BasicFragment.getImage();
         return null;
     }
 
